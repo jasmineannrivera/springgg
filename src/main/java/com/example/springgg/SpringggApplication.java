265ee2552b7a3121e6df6d8cb681056bb3b0a45d
Jasmine Rivera <jasminerivera@jasmines-mbp.corpaa.aa.com>
2021-08-19 16:53:50 -0500

    springggg

---
+++ src/main/java/com/example/springgg/SpringggApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringggApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringggApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springgg/SpringggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringggApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
